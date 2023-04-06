7ef19b3cea452b6f07b389807ec9bd0cf3f54b7b
Oliver03710 <jhee2619@gmail.com>
2023-04-06 21:12:30 +0900

    코드 일부 수정

--- ProjectMemo/Extensions/Date+Extension.swift
+++ ProjectMemo/Extensions/Date+Extension.swift
@@ -21,14 +21,15 @@ extension Date {
         return str
     }
     
-    static func -(recent: Date, previous: Date) -> (month: Int?, day: Int?, hour: Int?, minute: Int?, second: Int?) {
+    static func -(recent: Date, previous: Date) -> (year: Int?, month: Int?, day: Int?, hour: Int?, minute: Int?, second: Int?) {
+        let year = Calendar.current.dateComponents([.year], from: previous, to: recent).year
         let day = Calendar.current.dateComponents([.day], from: previous, to: recent).day
         let month = Calendar.current.dateComponents([.month], from: previous, to: recent).month
         let hour = Calendar.current.dateComponents([.hour], from: previous, to: recent).hour
         let minute = Calendar.current.dateComponents([.minute], from: previous, to: recent).minute
         let second = Calendar.current.dateComponents([.second], from: previous, to: recent).second
 
-        return (month: month, day: day, hour: hour, minute: minute, second: second)
+        return (year: year, month: month, day: day, hour: hour, minute: minute, second: second)
     }
     
     func setDateFormat() -> String {
